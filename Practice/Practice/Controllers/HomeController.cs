e563333fb275bfb061b7bc2c1eb951604568a5ed
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-11-19 18:08:21 +0400

    task added

---
+++ Practice/Practice/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.Models;
+
+namespace Practice.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product{
+                  Id = 1,
+                  Name = "paltaryuyan",
+                  Category = "ev eşyası",
+                  Description = "Description",
+                  Price = 599.99m,
+                  DiscountPrice = 99.99m,
+                  Color = "white"
+                
+                
+                },
+                    new Product{
+                  Id = 2,
+                  Name = "soyuducu",
+                  Category = "ev eşyası",
+                  Description = "Description",
+                  Price = 1299.99m,
+                  DiscountPrice = 399.99m,
+                  Color = "black"
+
+
+                }
+
+
+            };
+
+
+
+            return View(products);
+        }
+
+
+    }
+}
